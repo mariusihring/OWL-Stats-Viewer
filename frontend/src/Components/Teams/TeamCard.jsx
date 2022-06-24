885d9225758054bd80f53d23edc1ac27ca8361a3
Samael-codes <89946878+Samael-codes@users.noreply.github.com>
2022-06-25 00:23:08 +0200

    made text and buttons disappear under 600px viewport

--- frontend/src/Components/Teams/TeamCard.jsx
+++ frontend/src/Components/Teams/TeamCard.jsx
@@ -7,7 +7,7 @@ function TeamCard(team) {
     <div className='card'>
       <div>
         <img className={team.team.Name} src={team.team.Logo} alt='' />
-        <h3>{team.team.Name.toUpperCase()}</h3>
+        <h3 className='teamname'>{team.team.Name.toUpperCase()}</h3>
       </div>
     </div>
   )
