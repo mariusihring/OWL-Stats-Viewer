302bc7ff1bd7aa839f4e9f3321ec36283ebf9de6
Samael-codes <89946878+mariusihring@users.noreply.github.com>
2022-06-28 00:52:31 +0200

    fixed da index nicht -1 genommen wurde und es an index 21 nichts gibt im teams array

--- frontend/src/Components/Games/Gamecard/Gamecard.jsx
+++ frontend/src/Components/Games/Gamecard/Gamecard.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function GameCard(game, teams) {
   return (
     <div className='game-card'>
-      <h1>{game.teams[game.game.map_winner].Name}</h1>
+      <h1>
+        {game.teams[game.game.map_winner - 1].Name}
+        {' : '}
+        {game.teams[game.game.map_loser - 1].Name}
+      </h1>
     </div>
   )
 }
