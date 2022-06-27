ec5d85aebb04ca969374495eaedea60d5d08ca5e
Samael-codes <89946878+Samael-codes@users.noreply.github.com>
2022-06-27 21:14:20 +0200

    Filter Teams with buttons and show active filter with colored button

--- frontend/src/Components/Teams/Teams.jsx
+++ frontend/src/Components/Teams/Teams.jsx
@@ -4,30 +4,76 @@ import TeamCard from './TeamCard'
 import '../css/Teams.css'
 
 function Teams() {
+  const [filter, setFilter] = useState('all')
   const [teams, setTeams] = useState([])
+  const [active, setActive] = useState('all')
   useEffect(() => {
-    fetch('http://127.0.0.1:1337/getAllTeams', {})
-      .then(async (response) => response.json())
-      .then(async (response) => {
-        setTeams(response)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+    if (filter === 'all') {
+      fetch('http://127.0.0.1:1337/getAllTeams', {})
+        .then(async (response) => response.json())
+        .then(async (response) => {
+          setTeams(response)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+    if (
+      (filter === '2021') |
+      (filter === '2020') |
+      (filter === '2019') |
+      (filter === '2018')
+    ) {
+      fetch(`http://127.0.0.1:1337/getTeams/${filter}`, {})
+        .then(async (response) => response.json())
+        .then(async (response) => {
+          setTeams(response)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }, [filter])
+  function handleButtonClick(e) {
+    setFilter(e)
+    setActive(e)
+  }
 
   return (
     <div className='page'>
       <div className='header'>
         <h1>Teams</h1>
         <div className='year-buttons'>
-          <button className='test' onClick={handleButtonClick}>
+          <button
+            className={active === 'all' ? 'active' : null}
+            onClick={() => handleButtonClick('all')}
+          >
             All
           </button>
-          <button>2021</button>
-          <button>2020</button>
-          <button>2019</button>
-          <button>2018</button>
+          <button
+            className={active === '2021' ? 'active' : null}
+            onClick={() => handleButtonClick('2021')}
+          >
+            2021
+          </button>
+          <button
+            className={active === '2020' ? 'active' : null}
+            onClick={() => handleButtonClick('2020')}
+          >
+            2020
+          </button>
+          <button
+            className={active === '2019' ? 'active' : null}
+            onClick={() => handleButtonClick('2019')}
+          >
+            2019
+          </button>
+          <button
+            className={active === '2018' ? 'active' : null}
+            onClick={() => handleButtonClick('2018')}
+          >
+            2018
+          </button>
         </div>
       </div>
       <div className='grid'>
