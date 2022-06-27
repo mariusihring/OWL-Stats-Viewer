8bb19f131201d0ff6db604af0b09f244a72d4ae1
Samael-codes <89946878+mariusihring@users.noreply.github.com>
2022-06-28 00:54:26 +0200

    added css and styling so my eyes dont hurt

--- frontend/src/Components/Games/Gamecard/Gamecard.jsx
+++ frontend/src/Components/Games/Gamecard/Gamecard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import './Gamecard.css'
 function GameCard(game, teams) {
   return (
     <div className='game-card'>
-      <h1>
+      <h2>
         {game.teams[game.game.map_winner - 1].Name}
         {' : '}
         {game.teams[game.game.map_loser - 1].Name}
-      </h1>
+      </h2>
     </div>
   )
 }
