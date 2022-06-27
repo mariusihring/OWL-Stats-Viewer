3c591d6a8978a2e0d0766aef6cbb2589d7db4469
Samael-codes <89946878+mariusihring@users.noreply.github.com>
2022-06-27 22:15:04 +0200

    added underlining in "home" view and fixed content x axis scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,38 @@
-import logo from './logo.svg'
+import React, { useState } from 'react'
 import './App.css'
 import Teams from './Components/Teams/Teams'
 function App() {
+  const [activePage, setActivePage] = useState('teams')
+  function handleClick(page) {
+    setActivePage(page)
+  }
   return (
     <div className='App'>
       <div className='Navigation'>
-        <button>Games</button>
-        <button>Teams</button>
-        <button>Players</button>
-        <button>Heroes</button>
+        <button
+          className={activePage == 'games' ? 'activePage' : 'null'}
+          onClick={() => handleClick('games')}
+        >
+          Games
+        </button>
+        <button
+          className={activePage == 'teams' ? 'activePage' : 'null'}
+          onClick={() => handleClick('teams')}
+        >
+          Teams
+        </button>
+        <button
+          className={activePage == 'players' ? 'activePage' : 'null'}
+          onClick={() => handleClick('players')}
+        >
+          Players
+        </button>
+        <button
+          className={activePage == 'heroes' ? 'activePage' : 'null'}
+          onClick={() => handleClick('heroes')}
+        >
+          Heroes
+        </button>
       </div>
       <div className='Content'>
         <div>
