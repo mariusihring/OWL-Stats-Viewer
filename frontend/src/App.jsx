0b0de1963514cb9a81bea64146e8ddff5401509a
Samael-codes <89946878+mariusihring@users.noreply.github.com>
2022-06-28 20:09:42 +0200

    random

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,52 @@
-import React, { useState } from "react";
-import "./App.css";
-import Games from "./Components/Games/Games";
-import Teams from "./Components/Teams/teamOverview/Teams";
+import React, { useState } from 'react'
+import './App.css'
+import Games from './Components/Games/Games'
+import Teams from './Components/Teams/teamOverview/Teams'
 function App() {
-  const [activeContent, setActiveContent] = useState("games");
-  const [activePage, setActivePage] = useState("games");
+  const [activeContent, setActiveContent] = useState('games')
+  const [activePage, setActivePage] = useState('games')
   function handleClick(page) {
-    setActiveContent(page);
-    setActivePage(page);
+    setActiveContent(page)
+    setActivePage(page)
   }
   return (
-    <div className="App">
-      <div className="Navigation">
+    <div className='App'>
+      <div className='Navigation'>
         <button
-          className={activeContent === "games" ? "activePage" : "null"}
-          onClick={() => handleClick("games")}
+          className={activeContent === 'games' ? 'activePage' : 'null'}
+          onClick={() => handleClick('games')}
         >
           Games
         </button>
         <button
-          className={activeContent === "teams" ? "activePage" : "null"}
-          onClick={() => handleClick("teams")}
+          className={activeContent === 'teams' ? 'activePage' : 'null'}
+          onClick={() => handleClick('teams')}
         >
           Teams
         </button>
         <button
-          className={activeContent === "players" ? "activePage" : "null"}
-          onClick={() => handleClick("players")}
+          className={activeContent === 'players' ? 'activePage' : 'null'}
+          onClick={() => handleClick('players')}
         >
           Players
         </button>
         <button
-          className={activeContent === "heroes" ? "activePage" : "null"}
-          onClick={() => handleClick("heroes")}
+          className={activeContent === 'heroes' ? 'activePage' : 'null'}
+          onClick={() => handleClick('heroes')}
         >
           Heroes
         </button>
       </div>
-      <div className="Content">
+      <div className='Content'>
         <div>
-          {activePage === "games" && <Games />}
-          {activePage === "teams" && <Teams />}
-          {activePage === "players" && <Teams />}
-          {activePage === "players" && <Teams />}
+          {activePage === 'games' && <Games />}
+          {activePage === 'teams' && <Teams />}
+          {activePage === 'players' && <Teams />}
+          {activePage === 'players' && <Teams />}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
