fc6444e0ba426ca62764b82728cfd99db625c268
Marius Ihring <89946878+mariusihring@users.noreply.github.com>
2022-07-04 17:26:31 +0200

    removed old sql dump

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -16,8 +16,8 @@ function App() {
     <div className='w-screen overflow-x-hidden'>
       <div className='drawer'>
         <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
-        <div className='drawer-content flex flex-col'>
-          <div className='w-full navbar bg-base-300'>
+        <div className='drawer-content flex flex-col '>
+          <div className='w-full navbar bg-base-300 '>
             <div className='flex-none lg:hidden'>
               <label htmlFor='my-drawer-3' className='btn btn-square btn-ghost'>
                 <svg
